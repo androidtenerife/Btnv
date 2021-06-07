14369239286c5fb1c48d78e6d718f03a0c1ac719
Herbert Daniel Rodríguez <tenerife.developer@gmail.com>
2021-06-07 22:53:02 +0100

    Created a new Fragment State_Of_Windows.kt

---
+++ app/src/main/java/com/bitnovo/seleccion/herbert/btnv/StateOfWindowsViewModel.kt
@@ -0,0 +1,7 @@
+package com.bitnovo.seleccion.herbert.btnv
+
+import android.arch.lifecycle.ViewModel
+
+class StateOfWindowsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/bitnovo/seleccion/herbert/btnv/State_Of_Windows.kt
@@ -0,0 +1,33 @@
+package com.bitnovo.seleccion.herbert.btnv
+
+import android.arch.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class State_Of_Windows : Fragment() {
+
+    companion object {
+        fun newInstance() = State_Of_Windows()
+    }
+
+    private lateinit var viewModel: StateOfWindowsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.state__of__windows_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+            StateOfWindowsViewModel::class.java
+        )
+        // TODO: Use the ViewModel
+    }
+
+}
