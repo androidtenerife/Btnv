47cec801717d5c0c1d18cd822b42bc45e1562d10
Herbert Daniel Rodríguez <tenerife.developer@gmail.com>
2021-06-08 17:57:53 +0100

    Clean the project with the Xml resources.

--- app/src/main/java/com/bitnovo/seleccion/herbert/btnv/MainActivity.kt
+++ app/src/main/java/com/bitnovo/seleccion/herbert/btnv/MainActivity.kt
@@ -2,7 +2,6 @@ package com.bitnovo.seleccion.herbert.btnv
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
@@ -27,10 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
