3d4fd4585f13d23acdacaf077f98582ed0ce1e0c
Herbert Daniel Rodríguez <tenerife.developer@gmail.com>
2021-06-08 16:59:47 +0100

    Functions Ready's : Get state of windows, Get Winners, ResetWindows to open All.
    In the UI Get Winners show the Winners numbers Array(Int)

--- app/src/main/java/com/bitnovo/seleccion/herbert/btnv/ui/get_winners/Get_Winners.kt
+++ app/src/main/java/com/bitnovo/seleccion/herbert/btnv/ui/get_winners/Get_Winners.kt
@@ -6,7 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bitnovo.seleccion.herbert.btnv.R
+import android.widget.TextView
+import com.bitnovo.seleccion.herbert.btnv.databinding.GetWinnersFragmentBinding
 import com.bitnovo.seleccion.herbert.btnv.model.Hotel
 
 class Get_Winners : Fragment() {
@@ -15,25 +16,37 @@ class Get_Winners : Fragment() {
         fun newInstance() = Get_Winners()
     }
 
-    private lateinit var viewModel: GetWinnersViewModel
+    private lateinit var getWinnersViewModel: GetWinnersViewModel
+    private var _binding: GetWinnersFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.get__winners_fragment, container, false)
+        getWinnersViewModel =
+            ViewModelProvider(
+                this,
+                ViewModelProvider.NewInstanceFactory()
+            )
+                .get(GetWinnersViewModel::class.java)
+        _binding = GetWinnersFragmentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(
+        getWinnersViewModel = ViewModelProvider(
             this,
             ViewModelProvider.NewInstanceFactory()
         ).get(GetWinnersViewModel::class.java)
         // TODO: Use the ViewModel
-        Hotel.fakeWinners()
-        Hotel.getWinners()
+        val winners: TextView = binding.tvWinners
+        //To see the winners
+        // Fake winners is a test Hotel.fakeWinners()
+        winners.text = Hotel.getWinners().toString()
     }
 
 }
